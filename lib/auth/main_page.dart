fdc4ca1efc291e646bce447c5c4c4a8e3e6db3d8
Viktoria Henyk <viktoria.genuk@gmail.com>
2023-09-01 18:09:17 +0200

    Main Page

--- lib/auth/main_page.dart
+++ lib/auth/main_page.dart
@@ -1,1 +1,25 @@
+// ignore_for_file: camel_case_types
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:to_do_app/auth/auth_page.dart';
+
+class Main_Page extends StatelessWidget {
+  const Main_Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Home_Screen();
+          } else {
+            return Auth_Page();
+          }
+        },
+      ),
+    );
+  }
+}

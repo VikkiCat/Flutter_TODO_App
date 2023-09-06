04d1e7cf0132d0b888508ad1fb9943d870f33e8a
Viktoria Henyk <viktoria.genuk@gmail.com>
2023-09-06 19:39:16 +0200

    create users with firestore

---
+++ lib/data/firestor.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: camel_case_types, non_constant_identifier_names
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Firestore_Datasource {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<bool> CreateUser(String email) async {
+    try {
+      await _firestore
+          .collection('users')
+          .doc(_auth.currentUser!.uid)
+          .set({"id": _auth.currentUser!.uid, "email": email});
+      return true;
+    } catch (e) {
+      return true;
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 // ignore: depend_on_referenced_packages
-//import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:to_do_app/screen/add_note_screen.dart';
+import 'package:to_do_app/auth/main_page.dart';
+//import 'package:to_do_app/screen/add_note_screen.dart';
 //import 'package:to_do_app/auth/auth_page.dart';
-import 'package:to_do_app/widgets/task_widget.dart';
+//import 'package:to_do_app/widgets/task_widget.dart';
 
 void main() async {
-  //WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Add_Screen(),
+      home: Main_Page(),
     );
   }
 }
